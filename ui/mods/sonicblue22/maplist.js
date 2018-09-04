0e2b0bfb8bc2aee0e618a792687e29f1d5f3ffc4
SonicBlue22 <gcshotwell@gmail.com>
2018-09-04 08:58:49 -0400

    added map uploads to list

--- ui/mods/sonicblue22/maplist.js
+++ ui/mods/sonicblue22/maplist.js
@@ -1,18 +1,21 @@
 cShareSystems.load_pas("SonicBlue22", [
 "coui://ui/mods/SonicBlue22/systems/arena.pas",
+"coui://ui/mods/SonicBlue22/systems/cryos.pas",
 "coui://ui/mods/SonicBlue22/systems/daudet.pas",
+"coui://ui/mods/SonicBlue22/systems/destroyer.pas",
 "coui://ui/mods/SonicBlue22/systems/disperse.pas",
 "coui://ui/mods/SonicBlue22/systems/doov.pas",
+"coui://ui/mods/SonicBlue22/systems/ether.pas",
 "coui://ui/mods/SonicBlue22/systems/giga.pas",
 "coui://ui/mods/SonicBlue22/systems/kermall.pas",
 "coui://ui/mods/SonicBlue22/systems/mega_asteroids.pas",
+"coui://ui/mods/SonicBlue22/systems/quator.pas"
 "coui://ui/mods/SonicBlue22/systems/rice.pas",
 "coui://ui/mods/SonicBlue22/systems/ring.pas",
 "coui://ui/mods/SonicBlue22/systems/rings.pas",
+"coui://ui/mods/SonicBlue22/systems/ruins.pas",
+"coui://ui/mods/SonicBlue22/systems/sector_17.pas",
 "coui://ui/mods/SonicBlue22/systems/sula.pas",
 "coui://ui/mods/SonicBlue22/systems/thc.pas",
 "coui://ui/mods/SonicBlue22/systems/wreck.pas",
-"coui://ui/mods/SonicBlue22/systems/sector_17.pas",
-"coui://ui/mods/SonicBlue22/systems/cryos.pas",
-"coui://ui/mods/SonicBlue22/systems/quator.pas"
 ]);
